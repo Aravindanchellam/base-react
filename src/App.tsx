d10bf5cf289064a9fbbb7141ebded2e5ba24bf07
aravindan <aravindan1994c@gmail.com>
2024-04-01 22:11:58 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import MainRouter from './Router';
+import Header from './Common/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <MainRouter />
+    </div>
+  );
+}
+
+export default App;
